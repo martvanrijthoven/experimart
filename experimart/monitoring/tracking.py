97eb565cab942a5a8c6243a908d5958d8ea50d94
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-27 12:16:51 +0200

    add logging

--- experimart/monitoring/tracking.py
+++ experimart/monitoring/tracking.py
@@ -1,22 +1,31 @@
 import logging
+import sys
 from pathlib import Path
-from pprint import pprint
 
 import yaml
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(levelname)s][%(asctime)s] %(message)s", "%I:%M:%S")
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(formatter)
+
+
+logger.addHandler(stream_handler)
 
 
 class Tracker:
     def __init__(self, log_path):
         self._log_path = Path(log_path)
         self._log_path.mkdir(parents=True, exist_ok=True)
-        self._logger = logging.getLogger(__name__)
 
     @property
     def log_path(self):
         return self._log_path
 
     def update(self, statistics: dict):
-        self._logger.info(str(statistics))
+        logger.info(str(statistics))
 
     def save_parameters(self, parameters: dict):
         with open(self._log_path / "parameters.yml", "w") as outfile:
