9603ca06153ef2a13ae2959f08267979e537e7b9
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-05-28 12:30:25 +0200

    check for mode metric

--- experimart/monitoring/monitor.py
+++ experimart/monitoring/monitor.py
@@ -11,7 +11,8 @@ class PerformanceMonitor:
         statistics = {"epoch": epoch}
         for mode, metrics in self._metrics.items():
             for metric_key, metric in metrics.items():
-                statistics[f"{mode}_{metric_key}"] = metric(outputs[mode][metric_key])
+                if metric_key in outputs[mode]:
+                    statistics[f"{mode}_{metric_key}"] = metric(outputs[mode][metric_key])
 
         for tracker in self._trackers.values():
             tracker.update(statistics)
