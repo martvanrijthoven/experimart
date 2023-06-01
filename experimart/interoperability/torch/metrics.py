8d4bd8abee78537dcf97b3353e027d3cc0408ddf
martvanrijthoven <mart.vanrijthoven@gmail.com>
2023-06-01 18:24:56 +0200

    torch cm

--- experimart/interoperability/torch/metrics.py
+++ experimart/interoperability/torch/metrics.py
@@ -1,27 +1,38 @@
+from typing import List
+
 import numpy as np
 import torch
+from pycm import ConfusionMatrix
 
 
-class TorchConfusionMatrixMetric(torch.nn.Module):
+class TorchConfusionMatrixMetricCollector:
+    def __init__(self, metric: str):
+        self._metric = metric
+
+    def __call__(self, cms: List["TorchConfusionMatrixMetric"]):
+        return getattr(cms[-1].get_matrix(), self._metric)
+
+
+class TorchConfusionMatrixMetric:
     """
     Computes a confusion matrix for a multiclass segmentation task.
     """
 
-    def __init__(self, num_classes, device='cuda'):
+    def __init__(self, num_classes, device="cuda"):
         super().__init__()
         self.num_classes = num_classes
         self._device = device
         self.reset()
 
-    def forward(self, y_true, y_pred):
+    def __call__(self, y_true, y_pred):
         y_true = y_true.reshape(-1)
         y_pred = y_pred.reshape(-1)
 
         mask = y_true != 0
-        
+
         y_true = y_true[mask]
         y_pred = y_pred[mask]
-        
+
         # Compute the confusion matrix for the current batch using matrix operations
         y_true_onehot = torch.nn.functional.one_hot(
             y_true, num_classes=self.num_classes
@@ -29,17 +40,17 @@ class TorchConfusionMatrixMetric(torch.nn.Module):
         y_pred_onehot = torch.nn.functional.one_hot(
             y_pred, num_classes=self.num_classes
         ).float()
-        
-        # Update the confusion matrix for the current batch and accumulate it with the existing confusion matrix
-        self.cm += torch.matmul(y_true_onehot.t(), y_pred_onehot)
 
-    def get_matrix(self, reset=True):
-        # Cast the final confusion matrix to a NumPy array
-        cm = self.cm.cpu().numpy().astype(np.int64)
-        if reset:
-            self.reset()
+        # Update the confusion matrix for the current batch and accumulate it with the existing confusion matrix
+        self.values += torch.matmul(y_true_onehot.t(), y_pred_onehot)
+        return self
+
+    def get_matrix(self):
+        cm = ConfusionMatrix(self.values.cpu().numpy().astype(np.int64))
+        self.reset()
         return cm
 
     def reset(self):
-        self.cm = torch.zeros((self.num_classes, self.num_classes), device=self._device)
-        
+        self.values = torch.zeros(
+            (self.num_classes, self.num_classes), device=self._device
+        )
